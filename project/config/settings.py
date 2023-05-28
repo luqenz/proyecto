e087c8ae70d9b1e5b90ef4bd4902150a3fd43fc9
Lucas Avanzato <lucasavanzato[200~^[[200~git config --global user.email lucasavanzato@gmail.comgit config --global user.email lucasavanzato[200~^[[200~>
2023-05-28 12:16:14 -0300

    Creacion de proyecto django y app home

--- project/config/settings.py
+++ project/config/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import sys
+
+APLICACIONES = BASE_DIR / "apps"
+sys.path.append(str(APLICACIONES))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+
 ]
 
 MIDDLEWARE = [
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
